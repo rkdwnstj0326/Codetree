a5dfc59e6efa79c9090d089c981a727665446748
rkdwnstj0326 <rkdwnstj0326@gmail.com>
2026-08-10 00:40:30 +0900

    [Passed] 비교 연산 (47ms, 16MB)

---
+++ trail0/비교 연산/comparison-operator.py
@@ -0,0 +1,34 @@
+A, B = input().split()
+
+A = int(A)
+B = int(B)
+
+if A >= B:
+    print(1)
+else:
+    print(0)
+
+if A > B:
+    print(1)
+else:
+    print(0)
+
+if B >= A:
+    print(1)
+else:
+    print(0)
+
+if B > A:
+    print(1)
+else:
+    print(0)
+
+if A == B:
+    print(1)
+else:
+    print(0)
+
+if A != B:
+    print(1)
+else:
+    print(0)
